61c992db40dd38188770811e3c8aea1a26bc6bad
AlexQuidditch <alexquidditch@gmail.com>
2019-07-29 21:11:45 +0300

    improved examples

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,39 +1,417 @@
 <template>
   <div class="about">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
 
-    <div ref="scene" id="scene"></div>
+    <div ref="container" id="container"></div>
   </div>
 </template>
 
 <script lang="ts">
+import { Component, Vue, Watch } from "vue-property-decorator";
 
-import { Component, Vue, Watch } from 'vue-property-decorator';
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  DirectionalLight,
+  GridHelper,
+  AmbientLight,
+  Color,
+  PlaneBufferGeometry,
+  Mesh,
+  SpotLight,
+  LightShadow,
+  ShadowMaterial,
+  Vector3,
+  Line,
+  CatmullRomCurve3,
+  LineBasicMaterial,
+  BufferGeometry,
+  BoxBufferGeometry,
+  MeshLambertMaterial,
+  BufferAttribute
+} from 'three';
 
-import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { DragControls } from "three/examples/jsm/controls/DragControls";
+import { TransformControls } from "three/examples/jsm/controls/TransformControls";
 
 @Component({})
 export default class About extends Vue {
   positionZ: number = 5;
 
-  scene: any = {};
-  camera: any = {};
-  renderer: any = {};
+  // SCENE: Scene = {};
+  // CAMERA: PerspectiveCamera = {};
+  // RENDERER: WebGLRenderer = {};
+  // LIGHT: DirectionalLight = {};
+  // CONTROLS: OrbitControls = {};
+  // GRID: GridHelper = {};
+  // AMBIENT_LIGHT: AmbientLight = {};
 
-  @Watch('positionZ')
-  onPositionZChange (val: number) {
-    this.camera.position.z = val;
+  @Watch("positionZ")
+  onPositionZChange(val: number) {
+    // this.this.CAMERA.position.z = val;
   }
 
-  async mounted () {
+  async mounted() {
     await this.$nextTick();
 
-    const scene = document.getElementById('scene');
-    if (scene) {
-      // do some things
+    String.prototype.format = function() {
+      var str = this;
+      for (var i = 0; i < arguments.length; i++) {
+        str = str.replace("{" + i + "}", arguments[i]);
+      }
+      return str;
+    };
+
+    var container, stats;
+    var camera, scene, renderer;
+    var splineHelperObjects = [];
+    var splinePointsLength = 4;
+    var positions = [];
+    var point = new Vector3();
+
+    var geometry = new BoxBufferGeometry(20, 20, 20);
+    var transformControl;
+
+    var ARC_SEGMENTS = 200;
+
+    var splines = {};
+
+    var params = {
+      uniform: true,
+      tension: 5,
+      centripetal: true,
+      chordal: true,
+      addPoint: addPoint,
+      removePoint: removePoint,
+      exportSpline: exportSpline
+    };
+
+    init();
+    animate();
+
+    function init() {
+      container = document.getElementById("container");
+
+      scene = new Scene();
+      scene.background = new Color(0xf0f0f0);
+
+      camera = new PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        10000
+      );
+      camera.position.set(0, 250, 1000);
+      scene.add(camera);
+
+      scene.add(new AmbientLight(0xf0f0f0));
+      var light = new SpotLight(0xffffff, 1.5);
+      light.position.set(0, 1500, 200);
+      light.castShadow = true;
+      light.shadow = new LightShadow(
+        new PerspectiveCamera(70, 1, 200, 2000)
+      );
+      light.shadow.bias = -0.000222;
+      light.shadow.mapSize.width = 1024;
+      light.shadow.mapSize.height = 1024;
+      scene.add(light);
+
+      var planeGeometry = new PlaneBufferGeometry(2000, 2000);
+      planeGeometry.rotateX(-Math.PI / 2);
+      var planeMaterial = new ShadowMaterial({ opacity: 0.2 });
+
+      var plane = new Mesh(planeGeometry, planeMaterial);
+      plane.position.y = -200;
+      plane.receiveShadow = true;
+      scene.add(plane);
+
+      var helper = new GridHelper(2000, 100);
+      helper.position.y = -199;
+      helper.material.opacity = 0.25;
+      helper.material.transparent = true;
+      scene.add(helper);
+
+      //var axes = new AxesHelper( 1000 );
+      //axes.position.set( - 500, - 500, - 500 );
+      //scene.add( axes );
+
+      renderer = new WebGLRenderer();
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
+      renderer.shadowMap.enabled = true;
+      container.appendChild(renderer.domElement);
+
+      // stats = new Stats();
+      // container.appendChild( stats.dom );
+
+      // var gui = new GUI();
+
+      // gui.add( params, 'uniform' );
+      // gui.add( params, 'tension', 0, 1 ).step( 0.01 ).onChange( function ( value ) {
+      // 	splines.uniform.tension = value;
+      //   updateSplineOutline();
+      // });
+      // gui.add( params, 'centripetal' );
+      // gui.add( params, 'chordal' );
+      // gui.add( params, 'addPoint' );
+      // gui.add( params, 'removePoint' );
+      // gui.add( params, 'exportSpline' );
+      // gui.open();
+
+      // Controls
+      var controls = new OrbitControls(camera, renderer.domElement);
+      controls.damping = 0.2;
+      controls.addEventListener("change", render);
+
+      controls.addEventListener("start", function() {
+        cancelHideTransform();
+      });
+
+      controls.addEventListener("end", function() {
+        delayHideTransform();
+      });
+
+      transformControl = new TransformControls(camera, renderer.domElement);
+      transformControl.addEventListener("change", render);
+      transformControl.addEventListener("dragging-changed", function(event) {
+        controls.enabled = !event.value;
+      });
+      scene.add(transformControl);
+
+      // Hiding transform situation is a little in a mess :()
+      transformControl.addEventListener("change", function() {
+        cancelHideTransform();
+      });
+
+      transformControl.addEventListener("mouseDown", function() {
+        cancelHideTransform();
+      });
+
+      transformControl.addEventListener("mouseUp", function() {
+        delayHideTransform();
+      });
+
+      transformControl.addEventListener("objectChange", function() {
+        updateSplineOutline();
+      });
+
+      var dragcontrols = new DragControls(
+        splineHelperObjects,
+        camera,
+        renderer.domElement
+      ); //
+      dragcontrols.enabled = false;
+      dragcontrols.addEventListener("hoveron", function(event) {
+        transformControl.attach(event.object);
+        cancelHideTransform();
+      });
+
+      dragcontrols.addEventListener("hoveroff", function() {
+        delayHideTransform();
+      });
+
+      var hiding;
+
+      function delayHideTransform() {
+        cancelHideTransform();
+        hideTransform();
+      }
+
+      function hideTransform() {
+        hiding = setTimeout(function() {
+          transformControl.detach(transformControl.object);
+        }, 2500);
+      }
+
+      function cancelHideTransform() {
+        if (hiding) clearTimeout(hiding);
+      }
+
+      /*******
+       * Curves
+       *********/
+
+      for (var i = 0; i < splinePointsLength; i++) {
+        addSplineObject(positions[i]);
+      }
+
+      positions = [];
+
+      for (var i = 0; i < splinePointsLength; i++) {
+        positions.push(splineHelperObjects[i].position);
+      }
+
+      var geometry = new BufferGeometry();
+      geometry.addAttribute(
+        "position",
+        new BufferAttribute(new Float32Array(ARC_SEGMENTS * 3), 3)
+      );
+
+      var curve = new CatmullRomCurve3(positions);
+      curve.curveType = "catmullrom";
+      curve.mesh = new Line(
+        geometry.clone(),
+        new LineBasicMaterial({
+          color: 0xff0000,
+          opacity: 0.35
+        })
+      );
+      curve.mesh.castShadow = true;
+      splines.uniform = curve;
+
+      curve = new CatmullRomCurve3(positions);
+      curve.curveType = "centripetal";
+      curve.mesh = new Line(
+        geometry.clone(),
+        new LineBasicMaterial({
+          color: 0x00ff00,
+          opacity: 0.35
+        })
+      );
+      curve.mesh.castShadow = true;
+      splines.centripetal = curve;
+
+      curve = new CatmullRomCurve3(positions);
+      curve.curveType = "chordal";
+      curve.mesh = new Line(
+        geometry.clone(),
+        new LineBasicMaterial({
+          color: 0x0000ff,
+          opacity: 0.35
+        })
+      );
+      curve.mesh.castShadow = true;
+      splines.chordal = curve;
+
+      for (var k in splines) {
+        var spline = splines[k];
+        scene.add(spline.mesh);
+      }
+
+      load([
+        new Vector3(
+          289.76843686945404,
+          452.51481137238443,
+          56.10018915737797
+        ),
+        new Vector3(
+          -53.56300074753207,
+          171.49711742836848,
+          -14.495472686253045
+        ),
+        new Vector3(
+          -91.40118730204415,
+          176.4306956436485,
+          -6.958271935582161
+        ),
+        new Vector3(
+          -383.785318791128,
+          491.1365363371675,
+          47.869296953772746
+        )
+      ]);
+    }
+
+    function addSplineObject(position) {
+      var material = new MeshLambertMaterial({
+        color: Math.random() * 0xffffff
+      });
+      var object = new Mesh(geometry, material);
+
+      if (position) {
+        object.position.copy(position);
+      } else {
+        object.position.x = Math.random() * 1000 - 500;
+        object.position.y = Math.random() * 600;
+        object.position.z = Math.random() * 800 - 400;
+      }
+
+      object.castShadow = true;
+      object.receiveShadow = true;
+      scene.add(object);
+      splineHelperObjects.push(object);
+      return object;
+    }
+
+    function addPoint() {
+      splinePointsLength++;
+
+      positions.push(addSplineObject().position);
+
+      updateSplineOutline();
+    }
+
+    function removePoint() {
+      if (splinePointsLength <= 4) {
+        return;
+      }
+      splinePointsLength--;
+      positions.pop();
+      scene.remove(splineHelperObjects.pop());
+
+      updateSplineOutline();
+    }
+
+    function updateSplineOutline() {
+      for (var k in splines) {
+        var spline = splines[k];
+
+        var splineMesh = spline.mesh;
+        var position = splineMesh.geometry.attributes.position;
+
+        for (var i = 0; i < ARC_SEGMENTS; i++) {
+          var t = i / (ARC_SEGMENTS - 1);
+          spline.getPoint(t, point);
+          position.setXYZ(i, point.x, point.y, point.z);
+        }
+
+        position.needsUpdate = true;
+      }
+    }
+
+    function exportSpline() {
+      var strplace = [];
+
+      for (var i = 0; i < splinePointsLength; i++) {
+        var p = splineHelperObjects[i].position;
+        strplace.push("new Vector3({0}, {1}, {2})".format(p.x, p.y, p.z));
+      }
+
+      console.log(strplace.join(",\n"));
+      var code = "[" + strplace.join(",\n\t") + "]";
+      prompt("copy and paste code", code);
+    }
+
+    function load(new_positions) {
+      while (new_positions.length > positions.length) {
+        addPoint();
+      }
+      while (new_positions.length < positions.length) {
+        removePoint();
+      }
+      for (var i = 0; i < positions.length; i++) {
+        positions[i].copy(new_positions[i]);
+      }
+      updateSplineOutline();
+    }
+
+    function animate() {
+      requestAnimationFrame(animate);
+      render();
+      // stats.update();
+    }
+
+    function render() {
+      splines.uniform.mesh.visible = params.uniform;
+      splines.centripetal.mesh.visible = params.centripetal;
+      splines.chordal.mesh.visible = params.chordal;
+      renderer.render(scene, camera);
     }
   }
 }
+
 </script>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,20 +20,47 @@
 
 import { Component, Vue, Watch } from 'vue-property-decorator';
 
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, Mesh, DirectionalLight, MeshPhongMaterial } from 'three';
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  Mesh,
+  DirectionalLight,
+  MeshPhongMaterial,
+  GridHelper,
+  AmbientLight,
+  SpotLight,
+  LightShadow,
+  PlaneBufferGeometry,
+  ShadowMaterial,
+} from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 @Component({})
 export default class Home extends Vue {
   positionZ: number = 5;
 
-  scene: any = {};
-  camera: any = {};
-  renderer: any = {};
-  light: any = {};
+  SCENE: Scene = {};
+  CAMERA: PerspectiveCamera = {};
+  RENDERER: WebGLRenderer = {};
+  LIGHT: DirectionalLight = {};
+  CONTROLS: OrbitControls = {};
+  GRID: GridHelper = {};
+  AMBIENT_LIGHT: AmbientLight = {};
 
   @Watch('positionZ')
   onPositionZChange (val: number) {
-    this.camera.position.z = val;
+    this.CAMERA.position.z = val;
+  }
+
+  private initControls () {
+    this.CONTROLS = new OrbitControls(this.CAMERA, this.RENDERER.domElement);
+    this.CONTROLS.dampingFactor = 25;
+    this.CONTROLS.minPolarAngle = Math.PI * 1 / 4;
+    this.CONTROLS.maxPolarAngle = Math.PI * 3 / 4;
+    this.CONTROLS.update();
   }
 
   async mounted () {
@@ -41,36 +68,65 @@ export default class Home extends Vue {
 
     const scene = document.getElementById('scene');
   
-    const geometry = new BoxGeometry( 1, 2, 1);
+    const geometry = new BoxGeometry(1, 2, 1);
     const material = new MeshPhongMaterial({ color: 'gray' });
     const cube = new Mesh( geometry, material );
 
     if (scene) {
-      this.scene = new Scene();
-      this.camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-      this.renderer = new WebGLRenderer();
-      this.light = new DirectionalLight('#fff', 1);
+      this.SCENE = new Scene();
+      
+      this.CAMERA = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      this.CAMERA.position.z = 5;
 
-      this.renderer.setSize( window.innerWidth * 0.6, window.innerHeight * 0.6 );
-      this.renderer.setClearColor('white');
-  
-      this.light.position.set( -1, 2, 4 );
+      this.LIGHT = new DirectionalLight('#fff', 1);
+      this.LIGHT.position.set( 0, 1500, 200 );
+      this.LIGHT.castShadow = true;
+      this.LIGHT.shadow = new LightShadow( new PerspectiveCamera( 70, 1, 200, 2000 ) );
+      this.LIGHT.shadow.bias = - 0.000222;
+      this.LIGHT.shadow.mapSize.width = 1024;
+      this.LIGHT.shadow.mapSize.height = 1024;
+      this.SCENE.add(this.LIGHT);
+      
+      this.GRID = new GridHelper( 100, 100, 'red', 'green' );
+      this.GRID.position.y = -1;
+      this.GRID.material.opacity = 0.25;
+      this.GRID.material.transparent = false;
+      this.GRID.receiveShadow = true;
 
-      this.scene.add(this.light);
+      this.SCENE.add(this.GRID);
+
+      cube.castShadow = true;
+      cube.receiveShadow = true;
+
+      this.SCENE.add(cube);
+
+      this.RENDERER = new WebGLRenderer();
  
-      this.camera.position.z = 5;
-  
-      scene.appendChild( this.renderer.domElement );
+      this.RENDERER.setPixelRatio(window.devicePixelRatio);
+      this.RENDERER.setSize( window.innerWidth * 0.8, window.innerHeight * 0.8 );
+      this.RENDERER.setClearColor('white');
+      this.RENDERER.shadowMap.enabled = true;
 
-      this.scene.add(cube);
+      scene.appendChild( this.RENDERER.domElement );
+
+      this.initControls();
+
       const animate = () => {
         requestAnimationFrame( animate );
+        this.CONTROLS.update();
         cube.rotation.x += 0.01;
-        cube.rotation.y += 0.05;
-        this.renderer.render( this.scene, this.camera );
+        cube.rotation.y += 0.01;
+        this.RENDERER.render( this.SCENE, this.CAMERA );
       };
   
       animate();
+      
+      window.addEventListener('resize', () => {
+        this.CAMERA.aspect = window.innerWidth / window.innerHeight;
+        this.CAMERA.updateProjectionMatrix();
+
+        this.RENDERER.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
+      });
     } else {
       console.error('Scene element is missed');
     }
